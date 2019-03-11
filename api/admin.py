39cf2f62ea56d60132536b44cb9ca11065d67076
Matt Wiens <mwiens91@gmail.com>
2019-03-11 16:50:51 -0700

    Initial commit

---
+++ api/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Player, Game
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display=("name",)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display=("player1", "player2")
---
+++ api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Player(models.Model):
+    name = models.CharField(
+        max_length=200, unique=True, help_text="The player's name."
+    )
+
+class Game(models.Model):
+    player1 = models.ForeignKey(
+        Player,
+        on_delete=models.PROTECT,
+        related_name="player1",
+    )
+    player2 = models.ForeignKey(
+        Player,
+        on_delete=models.PROTECT,
+        related_name="player2",
+    )
+    player1_score = models.PositiveSmallIntegerField(default=0)
+    player2_score = models.PositiveSmallIntegerField(default=0)
+    datetime_played = models.DateTimeField(default=timezone.now)
