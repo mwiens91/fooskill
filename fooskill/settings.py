56f7c5c1e2f40ed87d6a04e8ab25a9768a86f566
Matt Wiens <mwiens91@gmail.com>
2019-03-11 23:20:17 -0700

    Add DRF stuff to installed_apps

--- fooskill/settings.py
+++ fooskill/settings.py
@@ -31,13 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "api",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
+    "crispy_forms",
+    "django_extensions",
+    "django_filters",
+    "drf_yasg",
+    "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
