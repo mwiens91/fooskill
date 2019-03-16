fe249dbcc5d6ffde72b9dcaaaa2d42541308fe66
Matt Wiens <mwiens91@gmail.com>
2019-03-16 00:00:43 -0700

    Start skeleton for frontend code

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            this be fooskill
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
