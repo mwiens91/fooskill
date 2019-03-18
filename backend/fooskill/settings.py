a8c17d745b883ea02f4f5a4375665289703046fa
Matt Wiens <mwiens91@gmail.com>
2019-03-17 22:49:44 -0700

    Make API read only if not authenticated

--- backend/fooskill/settings.py
+++ backend/fooskill/settings.py
@@ -155,6 +155,6 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
 }
