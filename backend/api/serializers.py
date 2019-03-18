c576d11037c7e40541dcd7293872853018d65097
Matt Wiens <mwiens91@gmail.com>
2019-03-17 18:43:31 -0700

    Flesh out API serlializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,7 @@
 """Contains serializers for models."""
 
+from django.conf import settings
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Game, Player, User
 
@@ -16,7 +18,10 @@ class PlayerSerializer(serializers.ModelSerializer):
     """A serializer for a player."""
 
     user = serializers.SlugRelatedField(
-        queryset=User.objects.all(), slug_field="username"
+        queryset=User.objects.all(),
+        slug_field="username",
+        required=False,
+        allow_null=True,
     )
 
     class Meta:
@@ -27,12 +32,22 @@ class PlayerSerializer(serializers.ModelSerializer):
 class GameSerializer(serializers.ModelSerializer):
     """A serializer for a game."""
 
+    datetime_played = serializers.DateTimeField(
+        default=lambda: timezone.now().strftime(
+            settings.REST_FRAMEWORK["DATETIME_FORMAT"]
+        ),
+        initial=lambda: timezone.now().strftime(
+            settings.REST_FRAMEWORK["DATETIME_FORMAT"]
+        ),
+    )
     winner = serializers.SlugRelatedField(
         queryset=Player.objects.all(), slug_field="name"
     )
     loser = serializers.SlugRelatedField(
         queryset=Player.objects.all(), slug_field="name"
     )
+    winner_score = serializers.IntegerField(min_value=0, initial=8)
+    loser_score = serializers.IntegerField(min_value=0, initial=0)
     submitted_by = serializers.SlugRelatedField(
         queryset=User.objects.all(), slug_field="username"
     )
