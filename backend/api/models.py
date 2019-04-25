2fa8886e6c992715c1dace76c6fab565833100b8
Matt Wiens <mwiens91@gmail.com>
2019-04-25 15:18:00 -0700

    Remove unnecessary import

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
-from django.utils import timezone
 
 
 class User(AbstractUser):
