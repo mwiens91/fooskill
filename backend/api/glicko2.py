3c9396d45563675e703c6ca84f63a361fd0ba406
Matt Wiens <mwiens91@gmail.com>
2019-04-27 16:05:10 -0700

    Add first draft of Glicko-2 algorithm.
    I think there's something wrong with it—it gives mostly correct results
    but a few things look suspect.

--- backend/api/glicko2.py
+++ backend/api/glicko2.py
@@ -9,8 +9,25 @@ from django.conf import settings
 
 # Glicko-2 parameters
 SCALE_FACTOR = 173.7178
-CONVERGENCE_TOLERANCE = 1e-6
+EPSILON = 1e-6
 TAU = settings.GLICKO2_SYSTEM_CONSTANT
+
+
+# Functions to convert between Glicko and Glicko-2 ratings
+def r_to_mu(r):
+    return (r - settings.GLICKO2_BASE_RATING) / SCALE_FACTOR
+
+
+def mu_to_r(mu):
+    return mu * SCALE_FACTOR + settings.GLICKO2_BASE_RATING
+
+
+def RD_to_phi(RD):
+    return RD / SCALE_FACTOR
+
+
+def phi_to_RD(phi):
+    return phi * SCALE_FACTOR
 
 
 # Functions used in Glicko-2 calculations
@@ -84,5 +101,70 @@ def calculate_player_rating(
         A three-tuple containing the player's new rating, rating
         deviation, and rating volatility.
     """
-    # TODO actually implement this
-    return (420, 0.69, 0.5)
+    # Deal with degenerate case first when no games have been played by
+    # the player
+    if opponent_rs is None:
+        RD_prime = phi_to_RD(sqrt(RD_to_phi(RD) ** 2 + sigma ** 2))
+
+        return (r, RD_prime, sigma)
+
+    # Calculate all ratings to Glicko-2 scale
+    mu = r_to_mu(r)
+    phi = RD_to_phi(RD)
+
+    opponent_mus = [r_to_mu(opponent_r) for opponent_r in opponent_rs]
+    opponent_phis = [RD_to_phi(opponent_RD) for opponent_RD in opponent_RDs]
+
+    # Compute v and delta
+    v = _v(mu, opponent_mus, opponent_phis)
+
+    delta = _delta(v, mu, scores, opponent_mus, opponent_phis)
+
+    # Compute new sigma value (Step 5 of Glicko-2 algorithm)
+    f = f_closure(delta, v, sigma, phi)
+
+    a = log(sigma ** 2)
+    A = a
+
+    if delta ** 2 > phi ** 2 + v:
+        B = log(delta ** 2 - phi ** 2 - v)
+    else:
+        k = 1
+
+        while f(a - k * TAU) < 0:
+            k += 1
+
+        B = a - k * TAU
+
+    fA = f(A)
+    fB = f(B)
+
+    while abs(B - A) > EPSILON:
+        C = A + (A - B) * fA / (fB - fA)
+        fC = f(C)
+
+        if fC * fB < 0:
+            A = B
+            fA = fB
+        else:
+            fA = fA / 2
+
+        B = C
+        fB = fC
+
+    sigma_prime = exp(A / 2)
+
+    # Compute new rating and rating deviation
+    phi_star = sqrt(phi ** 2 + sigma_prime ** 2)
+
+    phi_prime = 1 / sqrt(1 / phi_star ** 2 + 1 / v)
+    mu_prime = mu + _delta(
+        phi_prime ** 2, mu, scores, opponent_mus, opponent_phis
+    )
+
+    # Convert back to Glicko scale
+    r_prime = mu_to_r(mu_prime)
+    RD_prime = phi_to_RD(phi_prime)
+
+    # Return the new ratings
+    return (r_prime, RD_prime, sigma_prime)
