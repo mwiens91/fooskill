197baff625be69e737dc23bf1af02a0ee8d317cc
Matt Wiens <mwiens91@gmail.com>
2019-04-27 20:42:45 -0700

    Show leaderboard on frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+const API_BASE_URL = process.env.REACT_APP_FOOSKILL_API_URL;
+
 const urls = [
   {
     title: "admin page",
@@ -23,19 +25,50 @@ class App extends Component {
     super(props);
 
     this.state = {
-      urls
+      players: null,
+      urls: urls
     };
+
+    this.setPlayers = this.setPlayers.bind(this);
+  }
+
+  setPlayers(players) {
+    this.setState({ players });
+  }
+
+  componentDidMount() {
+    fetch(`${API_BASE_URL}/players`)
+      .then(response => response.json())
+      .then(response => this.setPlayers(response))
+      .catch(error => error);
   }
 
   render() {
+    const { players } = this.state;
+
+    if (!players) {
+      return null;
+    }
+
     return (
       <div className="App">
         <h1>FANTASTIC FOOSKILL FRONTEND</h1>
+
         {this.state.urls.map(item => (
           <div key={item.key}>
             <a href={item.url}>{item.title}</a>
           </div>
         ))}
+
+        <h2>PLAYAS</h2>
+
+        {players
+          .sort((p1, p2) => p1.rating < p2.rating)
+          .map((player, index) => (
+            <div key={player.id}>
+              {index + 1}. {player.name} {Math.round(player.rating)}
+            </div>
+          ))}
       </div>
     );
   }
