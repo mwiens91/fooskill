7b32f1532f7c616de24c046188f5311902654206
Matt Wiens <mwiens91@gmail.com>
2019-04-29 21:27:21 -0700

    Make SignInModal a functional component

--- frontend/src/components/SignInModal.js
+++ frontend/src/components/SignInModal.js
@@ -1,38 +1,27 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import Modal from "react-bootstrap/Modal";
 import SignInForm from "./SignInForm";
 
-class SignInModal extends Component {
-  render() {
-    return (
-      <Modal
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={this.props.show}
-        onHide={this.props.onHide}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Sign in</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <SignInForm
-            handleSubmit={(e, data) => {
-              this.props.handleSubmit(e, data);
-              this.props.onHide();
-            }}
-          />
-        </Modal.Body>
-      </Modal>
-    );
-  }
-}
+const SignInModal = ({ handleSubmit, onHide, show }) => (
+  <Modal
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    show={show}
+    onHide={onHide}
+  >
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">Sign in</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <SignInForm
+        handleSubmit={(e, data) => {
+          handleSubmit(e, data);
+          onHide();
+        }}
+      />
+    </Modal.Body>
+  </Modal>
+);
 
 export default SignInModal;
-
-SignInModal.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  onHide: PropTypes.func.isRequired,
-  show: PropTypes.func.isRequired
-};
