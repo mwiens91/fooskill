a7e0db524530bb1b4b9e9d85ec3b1b768ad33d51
Matt Wiens <mwiens91@gmail.com>
2019-04-29 21:10:01 -0700

    Add dev links to navbar dropdown

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -36,14 +36,16 @@ const FooskillNavbar = ({ loggedIn, user }) => (
             }
             id="basic-nav-dropdown"
           >
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
+            <NavDropdown.Item href={process.env.REACT_APP_FOOSKILL_ADMIN_URL}>
+              Admin portal
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
+            <NavDropdown.Item href={process.env.REACT_APP_FOOSKILL_API_URL}>
+              API root
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              href={process.env.REACT_APP_FOOSKILL_API_SPEC_URL}
+            >
+              API spec
             </NavDropdown.Item>
           </NavDropdown>
         </Nav>
